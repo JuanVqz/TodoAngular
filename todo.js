2e310e805fef1c31d9538339536bee55020f9216
juanvqz <juaraldo@hotmail.com>
2014-11-30 15:44:05 -0600

    Agregue el boton borrar tareas completas

--- todo.js
+++ todo.js
@@ -1,8 +1,20 @@
 function todoCtrl($scope) {
-	$scope.total = 4;
 
 	$scope.tareas = [
 		{text:'Aprender AngularJS', hecho:false},
 		{text:'Usar GitHub', hecho:false},
 	];
+
+	$scope.getTotalTareas = function () {
+		return $scope.tareas.length;
+	};
+
+	$scope.limpiarTareasCompletas = function () {
+		$scope.tareas
+	};
+
+	$scope.agregarTarea = function () {
+		$scope.tareas.push({text:$scope.nuevaTarea, hecho:false});
+		$scope.nuevaTarea = '';
+	};
 }
