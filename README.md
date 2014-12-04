63f77f4dae17274e5cc1b9f220e6b8ea6944330f
juanvqz <juaraldo@hotmail.com>
2014-12-03 21:28:12 -0600

    explicando en readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Este ejemplo es tomado de la pagina oficial de Angular.
 
-Lo explico en espaniol.
+Se trata de una lista de tareas donde por medio de angular se agregan 
+
+y se eliminan las tareas a la lista.
--- todo.js
+++ todo.js
@@ -3,14 +3,17 @@ function todoCtrl($scope) {
 	$scope.tareas = [
 		{text:'Aprender AngularJS', hecho:false},
 		{text:'Usar GitHub', hecho:false},
+		{text:'Crear App ', hecho:false},
 	];
 
 	$scope.getTotalTareas = function () {
 		return $scope.tareas.length;
 	};
 
-	$scope.limpiarTareasCompletas = function () {
-		$scope.tareas
+	$scope.limpiarTareaCompleta = function () {
+		$scope.tareas = _.filter($scope.tareas, function(tarea){
+			return !tarea.hecho;
+		})
 	};
 
 	$scope.agregarTarea = function () {
